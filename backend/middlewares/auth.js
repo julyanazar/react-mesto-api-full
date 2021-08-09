17dbc4f4b4fe1864d75f3d863a60b932126c2603
julyanazar <nnwrnuwr@gmail,com>
2021-08-09 09:15:57 +0400

    refactor: обрабатывать все ошибки централизованно

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Auth = require('../errors/Auth');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -6,9 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new Auth('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -17,9 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new Auth('Необходима авторизация');
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
